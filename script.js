7822cc90e673061305e88d6a0700d467e050ec16
Oscar Wani <oscarwani@oscars-mbp.attlocal.net>
2023-07-30 21:15:24 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -43,5 +43,12 @@
 // 4. Using the chosen approach (promise chaining or async/await), convert the response to json and return the data in its entirety
 
 
+async function getCatFacts(number) {
+    let response = await fetch('https://catfact.ninja/facts?limit=${number}')
+    console.log(response)
+    let json = await response.json()
+    return json
+}
+
 
 //END OF ASSIGNMENT
